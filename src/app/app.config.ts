65fda5330cb75e4a5f122b563941f6079f9d81ab
Andres Ceron <andres.ceron@burtcorp.com>
2025-04-12 19:36:44 +0200

    Add firebase firestore and update angular and node version
    When installing firebase it was needed to downgrade to angular 19.1.7 because of compatibility issues

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,8 @@ import Aura from '@primeng/themes/aura';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 
@@ -18,5 +20,7 @@ export const appConfig: ApplicationConfig = {
         },
       },
     }),
+    provideFirebaseApp(() => initializeApp({ projectId: "knowify-cafe", appId: "1:908367346368:web:ad6bd1a9dd3468639d4b25", storageBucket: "knowify-cafe.firebasestorage.app", apiKey: "AIzaSyAgjFGFWY8rE_s1iSPfZjR1E1CfDhaeaMo", authDomain: "knowify-cafe.firebaseapp.com", messagingSenderId: "908367346368", measurementId: "G-QJJ6FV828C" })),
+    provideFirestore(() => getFirestore()),
   ],
 };
